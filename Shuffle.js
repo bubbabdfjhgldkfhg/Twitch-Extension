ee8f4c69d1b92cac6d48ce2f851a73f59b356e0c
bubbabdfjhgldkfhg <36219743+bubbabdfjhgldkfhg@users.noreply.github.com>
2025-06-14 13:10:06 -0400

    Update Shuffle.js

--- Shuffle.js
+++ Shuffle.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shuffle
 // @namespace    https://github.com/bubbabdfjhgldkfhg/Twitch-Extension
-// @version      1.5
+// @version      1.6
 // @description  Adds a shuffle button to the Twitch video player
 // @updateURL    https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Shuffle.js
 // @downloadURL  https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Shuffle.js
