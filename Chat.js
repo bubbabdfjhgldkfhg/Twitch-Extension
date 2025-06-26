510a3cfb2fde204de002e5f8a5070916f1f7230a
bubbabdfjhgldkfhg <36219743+bubbabdfjhgldkfhg@users.noreply.github.com>
2025-06-26 16:06:56 -0400

    Fix tilde view for mention messages

--- Chat.js
+++ Chat.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat
 // @namespace    https://github.com/bubbabdfjhgldkfhg/Twitch-Extension
-// @version      2.12
+// @version      2.13
 // @description  Cleanup clutter from twitch chat
 // @updateURL    https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Chat.js
 // @downloadURL  https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Chat.js
@@ -32,7 +32,7 @@
                 tildeHeld = true;
                 const messages = document.querySelectorAll('.chat-line__message, .chat-line__status');
                 messages.forEach(message => {
-                    message.style.display = 'block';
+                    message.style.removeProperty('display');
                     message.style.opacity = brightness;
                 });
             }
