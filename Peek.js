016f78cd3bda6d5de828065447f1c59cba51a4aa
bubbabdfjhgldkfhg <36219743+bubbabdfjhgldkfhg@users.noreply.github.com>
2025-10-16 17:14:25 -0400

    Prevent Peek timer reset on repeated hover

--- Peek.js
+++ Peek.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Peek
 // @namespace    https://github.com/bubbabdfjhgldkfhg/Twitch-Extension
-// @version      0.5
+// @version      0.6
 // @description  Preview a Twitch channel stream when hovering channel links
 // @updateURL    https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Peek.js
 // @downloadURL  https://raw.githubusercontent.com/bubbabdfjhgldkfhg/Twitch-Extension/main/Peek.js
@@ -114,6 +114,10 @@
         return target;
     };
     const scheduleShow = (anchor, chan) => {
+        if (timer && pending && pending.chan === chan) {
+            pending.anchor = anchor;
+            return;
+        }
         pending = { anchor, chan };
         clearTimeout(timer);
         timer = setTimeout(() => {
